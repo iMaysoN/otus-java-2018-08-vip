403aad0835aa6909529f719d9b902766833244b5
valeriy.ivannikov <valeriy.ivannikov@team.wrike.com>
2018-09-04 22:36:19 +0300

    Obfuscation fix

--- viplessons/src/main/java/ru/otus/L20180801/Main.java
+++ viplessons/src/main/java/ru/otus/L20180801/Main.java
@@ -1,13 +1,7 @@
 package ru.otus.L20180801;
 
-import com.google.common.collect.ImmutableList;
-
-import java.util.Arrays;
-
 public class Main {
     public static void main(String... args) {
-        final ImmutableList<String> strings = ImmutableList.copyOf(Arrays.asList("One", "Two"));
-        System.out.println(strings);
-
+        System.out.println("Obfuscation alive! ALIVE!");
     }
 }
